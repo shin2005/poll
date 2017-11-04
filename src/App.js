68f8f050f009449639873e33f2b3ca53ee01d533
Harry Kwon <tarivid3@gmail.com>
2017-11-04 14:16:03 +0900

    harry first commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h5>hello!!!!</h5>
       </div>
     );
   }
