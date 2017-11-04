fd42ba4deaf333e41fd8f5900ea32a0bbecab332
sonic2005 <2005sonic@gmail.com>
2017-11-04 14:46:14 +0900

    ABCDEFG

--- src/App.js
+++ src/App.js
@@ -7,15 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 align = "center" className="Twin-kle">Twin-kle is great!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h5>hello!!!!</h5>
-        <h2>gun is the master</h2>
-        <h2>not really sonic i hate just kidding</h2>
       </div>
     );
   }
