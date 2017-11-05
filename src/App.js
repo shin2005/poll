4bea2569e2966550d56b786734d00585b37a95fd
Mikey Lee <mikey1384@gmail.com>
2017-11-05 13:57:42 +0900

    clicking radio buttons identifies which option was selected and when you click vote the option value is properly shown on alert popup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      checked: null
+    }
+  }
+
   render() {
+    const {checked} = this.state
     return (
       <div className="App">
         <h1 align = "center">This is a website for making polls and voting for them.</h1>
@@ -11,28 +19,55 @@ class App extends Component {
           <legend>What is your JavaScript library of choice?</legend>
           <form id="form1" name="form1">
             <label>
-              <input type="radio" name="Poll" value="mootools" id="Poll_0" />
+              <input
+                type="radio"
+                checked={checked === 'Mootools'}
+                onClick={() => this.setState({checked: 'Mootools'})}
+              />
               Mootools
             </label>
             <label>
-              <input type="radio" name="Poll" value="prototype" id="Poll_1" />
+              <input
+                type="radio"
+                checked={checked === 'Prototype'}
+                onClick={() => this.setState({checked: 'Prototype'})}
+              />
               Prototype
             </label>
             <label>
-              <input type="radio" name="Poll" value="jquery" id="Poll_2" />
+              <input
+                type="radio"
+                checked={checked === 'jQuery'}
+                onClick={() => this.setState({checked: 'jQuery'})}
+              />
               jQuery
             </label>
             <label>
-              <input type="radio" name="Poll" value="spry" id="Poll_3" />
+              <input
+                type="radio"
+                checked={checked === 'Spry'}
+                onClick={() => this.setState({checked: 'Spry'})}
+              />
               Spry
             </label>
             <label>
-              <input type="radio" name="Poll" value="other" id="Poll_4" />
+              <input
+                type="radio"
+                checked={checked === 'Other'}
+                onClick={() => this.setState({checked: 'Other'})}
+              />
               Other
             </label>
-           <button onClick={() => alert('Your vote was submitted!')}>Vote</button>
-         </form>
-       </fieldset>
+            <button onClick={
+              (event) => {
+                event.preventDefault() // to prevent default behavior of form submission. see what happens if you remove this line
+                alert(`Your vote was submitted! You voted ${checked}`)}
+              }
+            >
+              Vote
+            </button>
+          </form>
+        </fieldset>
       </div>
     );
   }
