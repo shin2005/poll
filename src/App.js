168455b9bdd430747b0f86a5c0eae43d13efa7d6
Your Name <you@example.com>
2017-11-05 09:51:12 +0900

    new letters

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ class App extends Component {
     return (
       <div className="App">
         <header style={{textAlign: 'left'}} className="App-header">
-          <h1 className="Twin-kle" color= "white">Twin-kle is amazing!</h1>
+          <h1 className="Twin-kle" color= "green">Twin-kle is amazing!</h1>
         </header>
         <div>
           <h1 style={{textAlign: 'center'}}>
             Twin-kle is an English academy located in Seoul!
           </h1>
+          <h2 style={{textAlign: 'right'}} className="Twin-kle"> YAYAYAYAYYAYAYYAYYAYAYAYYAYAYAY!!!!!</h2>
         </div>
       </div>
     );
