5cb68769d675a0713790ae0c083cf44d6424283c
Harry Kwon <tarivid3@gmail.com>
2017-11-11 14:53:24 +0900

    added graph

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      checked: null
+      checked: null,
+      dummyData: {
+        Mootools: 5,
+        Prototype: 3,
+        JQ: 13,
+        Spry: 4,
+        Other: 10
+      }
     }
   }
 
   render() {
-    const {checked} = this.state
+    const {checked, dummyData: {Mootools, Prototype, JQ, Spry, Other}} = this.state
+    const mootoolsPercentage = Mootools * 100 / (Mootools + Prototype + JQ + Spry + Other)
+    const prototypePercentage = Prototype * 100 / (Mootools + Prototype + JQ + Spry + Other)
+    const jqueryPercentage = JQ * 100 / (Mootools + Prototype + JQ + Spry + Other)
+    const spryPercentage = Spry * 100 / (Mootools + Prototype + JQ + Spry + Other)
+    const otherPercentage = Other * 100 / (Mootools + Prototype + JQ + Spry + Other)
     return (
       <div className="App">
-        <h1 align = "center">This is a website for making polls and voting for them.</h1>
+        <h1 align = "center" id="first_h1">This is a website for making polls and voting for them.</h1>
         <h3 align = "center">Go on, try voting for this sample poll!</h3>
         <fieldset>
           <legend>What is your JavaScript library of choice?</legend>
@@ -68,6 +80,29 @@ class App extends Component {
             </button>
           </form>
         </fieldset>
+        <div className="progress">
+          <div className="progress-bar" role="progressbar" style={{width: `${mootoolsPercentage}%`}} aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress-bar bg-success" role="progressbar" style={{width: `${prototypePercentage}%`}} aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress-bar bg-info" role="progressbar" style={{width: `${jqueryPercentage}%`}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress-bar bg-warning" role="progressbar" style={{width: `${spryPercentage}%`}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress-bar bg-danger" role="progressbar" style={{width: `${otherPercentage}%`}} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+
+        <div class="progress">
+          <div class="progress-bar bg-success" role="progressbar" style={{width: `${mootoolsPercentage}%`}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar bg-info" role="progressbar" style={{width: `${prototypePercentage}%`}} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar bg-warning" role="progressbar" style={{width: `${jqueryPercentage}%`}} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar bg-danger" role="progressbar" style={{width: `${spryPercentage}%`}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <div class="progress">
+            <div class="progress-bar bg-danger" role="progressbar" style={{width: `${otherPercentage}%`}} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
       </div>
     );
   }
