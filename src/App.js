42e4ab9b47ff215b92a8e70f23366ffffec0691c
BuildTools <unconfigured@null.spigotmc.org>
2017-11-11 15:07:19 +0900

    drastic changes and background

--- src/App.js
+++ src/App.js
@@ -13,61 +13,57 @@ class App extends Component {
     const {checked} = this.state
     return (
       <div className="App">
-        <h1 align = "center">This is a website for making polls and voting for them.</h1>
-        <h3 align = "center">Go on, try voting for this sample poll!</h3>
-        <fieldset>
-          <legend>What is your JavaScript library of choice?</legend>
-          <form id="form1" name="form1">
-            <label>
-              <input
-                type="radio"
-                checked={checked === 'Mootools'}
-                onClick={() => this.setState({checked: 'Mootools'})}
-              />
-              Mootools
-            </label>
-            <label>
-              <input
-                type="radio"
-                checked={checked === 'Prototype'}
-                onClick={() => this.setState({checked: 'Prototype'})}
-              />
-              Prototype
-            </label>
-            <label>
-              <input
-                type="radio"
-                checked={checked === 'jQuery'}
-                onClick={() => this.setState({checked: 'jQuery'})}
-              />
-              jQuery
-            </label>
-            <label>
-              <input
-                type="radio"
-                checked={checked === 'Spry'}
-                onClick={() => this.setState({checked: 'Spry'})}
-              />
-              Spry
-            </label>
-            <label>
-              <input
-                type="radio"
-                checked={checked === 'Other'}
-                onClick={() => this.setState({checked: 'Other'})}
-              />
-              Other
-            </label>
-            <button onClick={
-              (event) => {
-                event.preventDefault() // to prevent default behavior of form submission. see what happens if you remove this line
-                alert(`Your vote was submitted! You voted ${checked}`)}
-              }
-            >
-              Vote
-            </button>
-          </form>
-        </fieldset>
+        <legend>What is your JavaScript library of choice?</legend>
+        <form id="form1" name="form1">
+           <label>
+             <input
+              type="radio"
+              checked={checked === 'Mootools'}
+              onClick={() => this.setState({checked: 'Mootools'})}
+            />
+            Mootools
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={checked === 'Prototype'}
+              onClick={() => this.setState({checked: 'Prototype'})}
+            />
+            Prototype
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={checked === 'jQuery'}
+              onClick={() => this.setState({checked: 'jQuery'})}
+            />
+            jQuery
+          </label>
+          <label>
+            <input
+              type="radio"
+              checked={checked === 'Spry'}
+              onClick={() => this.setState({checked: 'Spry'})}
+            />
+            Spry
+          </label>
+          <label>
+             <input
+              type="radio"
+              checked={checked === 'Other'}
+              onClick={() => this.setState({checked: 'Other'})}
+            />
+            Other
+          </label>
+          <button onClick={
+            (event) => {
+              event.preventDefault() // to prevent default behavior of form submission. see what happens if you remove this line
+              alert(`Your vote was submitted! You voted ${checked}`)}
+            }
+          >
+            Vote
+          </button>
+        </form>
       </div>
     );
   }
