9c3031e6774660b9c7cf2936b44b185a736569cf
Mikey Lee <mikey1384@gmail.com>
2017-12-09 17:49:23 +0900

    change

---
+++ src/components/VoteOptions.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+export default function VoteOptions({
+    handleVote,
+    onClickMooTools,
+    onClickProtoType,
+    onClickJQ,
+    onClickSpry,
+    onClickReact,
+    onClickOther,
+    votedItem
+  }) {
+  return (
+    <fieldset>
+      <legend>What is your JavaScript library of choice? Feel Free To Vote AnyThing you Want :)</legend>
+      <form id="form1" name="form1">
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'Mootools'}
+            onClick={onClickMooTools}
+          />
+          Mootools
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'Prototype'}
+            onClick={onClickProtoType}
+          />
+          Prototype
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'JQ'}
+            onClick={onClickJQ}
+          />
+          jQuery
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'Spry'}
+            onClick={onClickSpry}
+          />
+          Spry
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'React'}
+            onClick={onClickReact}
+          />
+          React
+        </label>
+        <label>
+          <input
+            type="radio"
+            checked={votedItem === 'Other'}
+            onClick={onClickOther}
+          />
+          Other
+        </label>
+        <button disabled={!votedItem} onClick={handleVote} className="submit_vote">
+          Vote
+        </button>
+      </form>
+    </fieldset>
+  )
+}
