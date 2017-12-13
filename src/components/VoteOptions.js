f16bd1c28f6d66369748cbc09cb9b83ebc8abfd8
BuildTools <unconfigured@null.spigotmc.org>
2017-12-13 21:03:08 +0900

    nothing changed

--- src/components/VoteOptions.js
+++ src/components/VoteOptions.js
@@ -11,7 +11,7 @@ export default function VoteOptions({
     votedItem
 }) {
     return (
-        <fieldset>
+      <fieldset>
         <legend>What is your JavaScript library of choice? Feel Free To Vote AnyThing you Want :)</legend>
         <form id="form1" name="form1">
           <label>
@@ -62,10 +62,10 @@ export default function VoteOptions({
             />
             Other
           </label>
-          <button disabled={!votedItem} onClick={handleVote} className="submit_vote">
+        <button disabled={!votedItem} onClick={handleVote} className="submit_vote">
             Vote
-          </button>
-        </form>
-      </fieldset>
-    )
+        </button>
+      </form>
+    </fieldset>
+  )
 }
