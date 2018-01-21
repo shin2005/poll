7bbb2d993fc1665b8420938f2d43d62a3b45edd5
mikey1384 <mikey1384@gmail.com>
2018-01-21 21:47:27 +0700

    multiple sample polls

--- src/containers/Body/Poll/index.js
+++ src/containers/Body/Poll/index.js
@@ -4,16 +4,7 @@ import PieChart from 'react-minimal-pie-chart';
 
 export default class Poll extends Component {
   render() {
-    const poll = {
-      title: 'This is a sample poll',
-      options: [
-        { id: 1, label: 'Data 1', value: 20, color: 'red' },
-        { id: 2, label: 'Data 2', value: 60, color: 'yellow' },
-        { id: 3, label: 'Data 3', value: 30, color: 'orange' },
-        { id: 4, label: 'Data 4', value: 20, color: 'blue' },
-        { id: 5, label: 'Data 5', value: 10, color: 'green' }
-      ]
-    }
+    const {poll} = this.props
     return (
       <div style={{ width: '100%', padding: '1rem' }}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
--- src/containers/Body/index.js
+++ src/containers/Body/index.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
 import Poll from './Poll';
+import URL from '../../constants/URL';
+import request from 'axios';
 
 export default class Body extends Component {
+  async componentDidMount() {
+    const { data } = await request.get(`${URL}/vote`);
+    console.log(data);
+  }
+
   render() {
+    const polls = [
+      {
+        id: 1,
+        title: 'This is a sample poll',
+        options: [
+          { id: 1, label: 'Data 1', value: 20, color: 'red' },
+          { id: 2, label: 'Data 2', value: 60, color: 'yellow' },
+          { id: 3, label: 'Data 3', value: 30, color: 'orange' },
+          { id: 4, label: 'Data 4', value: 20, color: 'blue' },
+          { id: 5, label: 'Data 5', value: 10, color: 'green' }
+        ]
+      },
+      {
+        id: 2,
+        title: 'This is a sample poll',
+        options: [
+          { id: 1, label: 'Data 1', value: 20, color: 'red' },
+          { id: 2, label: 'Data 2', value: 60, color: 'yellow' },
+          { id: 3, label: 'Data 3', value: 30, color: 'orange' },
+          { id: 4, label: 'Data 4', value: 20, color: 'blue' },
+          { id: 5, label: 'Data 5', value: 10, color: 'green' }
+        ]
+      },
+      {
+        id: 3,
+        title: 'This is a sample poll',
+        options: [
+          { id: 1, label: 'Data 1', value: 20, color: 'red' },
+          { id: 2, label: 'Data 2', value: 60, color: 'yellow' },
+          { id: 3, label: 'Data 3', value: 30, color: 'orange' },
+          { id: 4, label: 'Data 4', value: 20, color: 'blue' },
+          { id: 5, label: 'Data 5', value: 10, color: 'green' }
+        ]
+      }
+    ];
     return (
       <div
         style={{
@@ -12,7 +54,9 @@ export default class Body extends Component {
         }}
       >
         <div style={{ width: '80%' }}>
-          <Poll />
+          {polls.map(poll => {
+            return <Poll key={poll.id} poll={poll} />;
+          })}
         </div>
       </div>
     );
